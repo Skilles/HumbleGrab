586412fe97d7fc0e6c7dd6a80b19bf43ccf4f4b1
Skilles <haloeer1@gmail.com>
2022-07-30 02:06:58 -0400

    Change core project folder structure

---
+++ HumbleGrab.Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using Config.Net;
+using HumbleGrab.Clients;
+using HumbleGrab.Options;
+
+var optionsPath = AppDomain.CurrentDomain.BaseDirectory + @"\config.yaml";
+
+var options = new ConfigurationBuilder<IHumbleOptions>()
+    .UseYamlFile(optionsPath)
+    .Build();
+
+var factory = new HumbleClientFactory(options);
+
+using var client = factory.CreateClient();
+
+Console.WriteLine("Starting...");
+if (options.AsyncMode)
+{
+    await client.StartAsync();
+}
+else
+{
+    client.Start();
+}
+Console.WriteLine("Finished!");
